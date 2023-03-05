c2e6bf18e5ab48c784c1149a3c4cf3bbf5ff0e6b
Sally Diament <sally.barzilai@gmail.com>
2023-03-04 22:24:05 -0300

    test: passing test_cria_produto req1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,5 +2,7 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    result = Product('a', 'a', '12/2/22', '12/2/22', 5, 'a')
-    assert result is None
+    result = f"{Product('1', 'a', 'a', '12/2/22', '12/2/22', 5, 'a')}"
+    exp = "O produto a fabricado em 12/2/22 por a com validade "
+    exp2 = "até 12/2/22 precisa ser armazenado a."
+    assert result == exp+exp2
