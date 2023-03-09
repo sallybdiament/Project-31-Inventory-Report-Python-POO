21e64d5b2431cb33ee73dace7ef3fd020b7db59d
Sally Diament <sally.barzilai@gmail.com>
2023-03-08 22:10:54 -0300

    corrigindo flake8

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,25 +7,36 @@ import xml.etree.ElementTree as ET
 
 class Inventory:
     @staticmethod
-    def import_data(path, relatoryType):
+    def read_xml_file(path):
+        list_file = []
+        with open(path) as file:
+            readXML = ET.parse(file)
+            iterableResult = readXML.getroot()
+            for item in iterableResult:
+                row = {}
+                for line in item:
+                    row[line.tag] = line.text
+            list_file.append(row)
+        return list_file
+
+    @staticmethod
+    def read_file(path):
         list_file = []
         if path.endswith(".csv"):
             with open(path) as file:
                 readerCSV = csv.DictReader(file)
                 for item in readerCSV:
                     list_file.append(item)
-        elif path.endswith(".xml"):
-            with open(path) as file:
-                readXML = ET.parse(file)
-                iterableResult = readXML.getroot()
-                for item in iterableResult:
-                    row = {}
-                    for line in item:
-                        row[line.tag] = line.text
-                    list_file.append(row)
-        else:
+        elif path.endswith(".json"):
             with open(path) as file:
                 list_file = json.load(file)
+        else:
+            list_file = Inventory.read_xml_file(path)
+        return list_file
+
+    @staticmethod
+    def import_data(path, relatoryType):
+        list_file = Inventory.read_file(path)
         if relatoryType == 'completo':
             return CompleteReport.generate(list_file)
         else:
